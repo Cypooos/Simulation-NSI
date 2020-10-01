c4eb24f028c4755a4539de81d88a3f6ec27b00c9
marcg-92250 <71487632+marcg-92250@users.noreply.github.com>
2020-10-01 16:31:23 +0200

    pb mouton
    oui c ce que g ecrit avant, j'arrive a trier la liste d'objet en fonction de la quantite mais je comprend pas pk il se deplace en diagonal ensuite

--- main.py
+++ main.py
@@ -3,8 +3,9 @@ from GUI import PygameGui
 
 
 if __name__ == '__main__':
-    monde = Monde((50,50))
+    monde = Monde((20,20))
     gui = PygameGui(monde,(700,700))
     gui.start()
     print("Programme fini")
+
 
